b7de9d63fe2c92e3535ad0e10b2359ff0e280f7c
TheMoys <diegomoys01@gmail.com>
2024-05-22 23:07:47 +0200

    feat: Add endpoint to update a game

--- game_galaxy_back/src/routes/gamesroutes.ts
+++ game_galaxy_back/src/routes/gamesroutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getgames } from '../controllers/gamescontroller';
 import { createGame } from '../controllers/gamescontroller';
+import { updateGame } from '../controllers/gamescontroller';
 
 const router = Router();
 
 router.get('/', getgames);
 router.post('/', createGame);
+router.put('/:id', updateGame);
 
 export default router;
