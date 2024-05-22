34a3dd1878df0d9043a059ae42ecafe76bad0ddd
TheMoys <diegomoys01@gmail.com>
2024-05-22 23:08:35 +0200

    feat: Add endpoint to delete a game

--- game_galaxy_back/src/routes/gamesroutes.ts
+++ game_galaxy_back/src/routes/gamesroutes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { getgames } from '../controllers/gamescontroller';
 import { createGame } from '../controllers/gamescontroller';
 import { updateGame } from '../controllers/gamescontroller';
+import { deleteGame } from '../controllers/gamescontroller';
 
 const router = Router();
 
 router.get('/', getgames);
 router.post('/', createGame);
 router.put('/:id', updateGame);
+router.delete('/:id', deleteGame);
 
 export default router;
