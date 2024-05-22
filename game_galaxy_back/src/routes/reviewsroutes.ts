f7ee44748899a292fa3d1f3ba780951a35cc8eb2
Diegoza04 <diego.barraza@alumnos.uneatlantico.es>
2024-05-22 23:07:58 +0200

    Creando la ruta y el metodo de Put en la coleccion Reviews

--- game_galaxy_back/src/routes/reviewsroutes.ts
+++ game_galaxy_back/src/routes/reviewsroutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { getReviews, createReview } from '../controllers/reviewsController';
+import { getReviews } from '../controllers/reviewsController';
+import { createReview } from '../controllers/reviewsController';
+import { updateReview } from '../controllers/reviewsController';
 
 const router = Router();
 
 router.get('/', getReviews);
 router.post('/', createReview);
+router.put('/:id', updateReview);
+
 
 export default router;
