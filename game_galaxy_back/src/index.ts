ad70230f678f4f228677cf2eb470836701e175e0
Diegoza04 <diego.barraza@alumnos.uneatlantico.es>
2024-05-22 14:21:01 +0200

    creando la opcion de crear las colecciones automaticamente

--- game_galaxy_back/src/index.ts
+++ game_galaxy_back/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import db from './config/db';
+import { MongoClient, Db } from 'mongodb';
 import gamesRouter from './routes/gamesroutes';
 import reviewsRouter from './routes/reviewsroutes';
 import usersRouter from './routes/usersroutes';
@@ -8,8 +8,7 @@ import usersRouter from './routes/usersroutes';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); 
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/games', gamesRouter);
@@ -20,13 +19,36 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Games API');
 });
 
-db.once('open', () => {
-  console.log('Conectado a MongoDB. Iniciando servidor Express...');
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-});
 
-db.on('error', (error) => {
-  console.error('Error de conexión a MongoDB:', error);
-});
+async function createCollections(db: Db): Promise<void> {
+  const collections = ['games', 'reviews', 'users'];
+
+  for (const collectionName of collections) {
+    try {
+      await db.createCollection(collectionName);
+      console.log(`Colección ${collectionName} creada`);
+    } catch (error: any) {
+      if (error.code === 48) {
+        console.log(`La colección ${collectionName} ya existe`);
+      } else {
+        console.error(`Error creando la colección ${collectionName}:`, error);
+      }
+    }
+  }
+}
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then(client => {
+    const db = client.db('game_galaxy');
+    createCollections(db).then(() => {
+      console.log('Conectado a MongoDB. Iniciando servidor Express...');
+      app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+    }).catch(error => {
+      console.error('Error al crear colecciones:', error);
+    });
+  })
+  .catch(error => {
+    console.error('Error de conexión a MongoDB:', error);
+  });
