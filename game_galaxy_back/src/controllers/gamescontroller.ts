8dedca44ab30bdf24137db6639f329aa086c8c9c
TheMoys <diegomoys01@gmail.com>
2024-05-22 22:35:53 +0200

    feat: Add endpoint to create a new game

--- game_galaxy_back/src/controllers/gamescontroller.ts
+++ game_galaxy_back/src/controllers/gamescontroller.ts
@@ -9,3 +9,14 @@ export const getgames = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Error al obtener los videojuegos', error });
   }
 };
+
+export const createGame = async (req: Request, res: Response) => {
+  try {
+    const newGame = new Game(req.body)
+    await newGame.save()
+    res.status(201).json(newGame)
+  } catch (error: any) {
+    console.error('Error al crear el juego:', error)
+    res.status(500).json({ message: 'Error al crear el juego', error })
+  }
+}
--- game_galaxy_back/src/routes/gamesroutes.ts
+++ game_galaxy_back/src/routes/gamesroutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { getgames } from '../controllers/gamescontroller';
+import { createGame } from '../controllers/gamescontroller';
 
 const router = Router();
 
 router.get('/', getgames);
+router.post('/', createGame);
 
 export default router;
